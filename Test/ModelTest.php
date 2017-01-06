120fb577d07c6031f654c0b1f6ca3ac0c8a519e2
marijanafilipovic <marijana.beograd@gmail.com>
2017-01-06 10:51:39 +0100

    commit message

---
+++ Test/ModelTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marijana Marfi
+ * Date: 14/12/2016
+ * Time: 9:56 PM
+ */
+use classes\Models;
+class ModelTest extends PHPUnit_Framework_TestCase {
+
+
+
+}
---
+++ Test/usersTest.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marijana Marfi
+ * Date: 14/12/2016
+ * Time: 9:55 PM
+ */
+
+class usersTest extends PHPUnit_Framework_TestCase {
+
+}
