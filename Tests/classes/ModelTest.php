de9a3c577092cdb5871c0f8bd05d00f88e490778
marijanafilipovic <marijana.beograd@gmail.com>
2017-03-15 13:52:48 +0100

    Update ModelTest.php

--- Tests/classes/ModelTest.php
+++ Tests/classes/ModelTest.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Marijana
- * Date: 15.3.2017.
- * Time: 12.05
- */
+
 
 namespace Tests;
 require dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR .'autoload.php';
@@ -23,12 +18,9 @@ class ModelTest extends TestCase
             ->disallowMockingUnknownTypes()
             ->getMock();
 
-        // Configure the stub.
         $stub->method('getInstance')
             ->will($this->returnSelf());
 
-        // Calling $stub->doSomething() will now return
-        // 'foo'.
         $this->assertSame($stub, $stub->getInstance());
     }
 
