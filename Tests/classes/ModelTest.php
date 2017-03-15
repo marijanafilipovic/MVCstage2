6bb92e0ba2f63e89dc3824d3ab5c88c446231b09
marijanafilipovic <marijana.beograd@gmail.com>
2017-03-15 13:25:22 +0100

    Commit change to new views branch

--- Tests/classes/ModelTest.php
+++ Tests/classes/ModelTest.php
@@ -7,11 +7,32 @@
  */
 
 namespace Tests;
-
+require dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR .'autoload.php';
 
 use PHPUnit\Framework\TestCase;
 
 class ModelTest extends TestCase
 {
 
+    public function testIsConnectPassed()
+    {
+        $stub = $this->getMockBuilder('Connect')
+            ->disableOriginalConstructor()
+            ->disableOriginalClone()
+            ->disableArgumentCloning()
+            ->disallowMockingUnknownTypes()
+            ->getMock();
+
+        // Configure the stub.
+        $stub->method('getInstance')
+            ->will($this->returnSelf());
+
+        // Calling $stub->doSomething() will now return
+        // 'foo'.
+        $this->assertSame($stub, $stub->getInstance());
+    }
+
+    public function testCheckName(){
+
+    }
 }
